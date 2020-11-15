0ab9a8d2a4925bcb41798271b8fa0d956f5523f5
hiendbui <hiendbui0@gmail.com>
2020-11-14 22:42:48 -0800

    Finish recursion problems

--- intro_js_exercises/phase_4_recursion.js
+++ intro_js_exercises/phase_4_recursion.js
@@ -46,3 +46,60 @@ const deepDup = function (arr) {
 }
 
 // console.log(deepDup([5,6,7]))
+
+const bsearch = function (arr, target) {
+    if (arr === []) return -1;
+    if (arr.length === 1 && arr[0] !== target) return -1;
+
+    const midIdx = Math.floor(arr.length/2);
+    const mid = arr[midIdx]
+
+    if (mid === target) {
+        return midIdx;
+    } else if (target < mid) {
+        const left = arr.slice(0, midIdx)
+        return bsearch(left, target);
+    } else {
+        const right = arr.slice(midIdx);
+        const result = bsearch(right, target);
+        return result === -1 ? -1 : result + midIdx ;
+    };
+}
+
+// console.log(bsearch([0, 1, 4, 5, 7, 11, 34, 39, 51], 34));
+// console.log(bsearch([0, 1, 4, 5, 7, 11, 34, 39, 51], 2));
+
+const mergeSort = function (arr) {
+    if (arr.length <= 1) return arr;
+
+    const mid = Math.floor(arr.length/2);
+    const left = arr.slice(0, mid);
+    const right = arr.slice(mid);
+
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+const merge = function (left, right) {
+    const newArr = [];
+
+    while (left.length > 0 && right.length > 0) {
+        left[0] < right[0] ? newArr.push(left.shift()) : newArr.push(right.shift());
+    };
+
+    return newArr.concat(left.concat(right));
+}
+
+// console.log(mergeSort([3,1,6,10,43,23,1,4]))
+
+const subsets = function (arr) {
+    if (arr.length === 0) {
+        return [[]];
+    }
+
+    const subSubset = subsets(arr.slice(1));
+
+    const withFirst = subSubset.map(sub => [arr[0]].concat(sub));
+    return subSubset.concat(withFirst);
+}
+
+console.log(subsets([1,2,3]))
